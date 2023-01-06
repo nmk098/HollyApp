28474960447e1956cffec22db21657fbedefbd5c
nmk098 <nkuryumov@gmail.com>
2023-01-06 23:27:43 +0400

    add view and first button to screen set animation for button and view

---
+++ extension + UIButton.swift
@@ -0,0 +1,23 @@
+//
+//  extension + UIButton.swift
+//  HollyApp
+//
+//  Created by Никита Курюмов on 02.01.23.
+//
+
+
+import UIKit
+
+extension UIButton {
+    func pulsate() {
+        let springAnimation = CASpringAnimation(keyPath: "transform.scale")
+        springAnimation.fromValue = 0.95
+        springAnimation.toValue = 1
+        springAnimation.duration = 0.6
+        springAnimation.autoreverses = true
+        springAnimation.repeatCount = 2
+        springAnimation.initialVelocity = 0.5
+        springAnimation.damping = 1
+        layer.add(springAnimation, forKey: nil)
+    }
+}
